8482bc6d19d4bb5c936952e2f02ee948424bfd13
ezequielalos-sketch <ezequielalos@gmail.com>
2026-03-30 20:10:48 -0300

    Create NewcomDatabase for local data management

---
+++ app/src/main/java/com/pilar/newcomapp/data/local/NewcomDatabase.kt
@@ -0,0 +1,18 @@
+package com.pilar.newcomapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.pilar.newcomapp.data.local.dao.PartidoDao
+import com.pilar.newcomapp.data.local.dao.RotacionDao
+import com.pilar.newcomapp.data.local.entity.PartidoEntity
+import com.pilar.newcomapp.data.local.entity.RotacionActualEntity
+
+@Database(
+    entities = [PartidoEntity::class, RotacionActualEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class NewcomDatabase : RoomDatabase() {
+    abstract fun partidoDao(): PartidoDao
+    abstract fun rotacionDao(): RotacionDao
+}
