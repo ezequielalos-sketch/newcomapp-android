b6d6da7a8cdd1b68eed92327eb5f298ea6f884cd
ezequielalos-sketch <ezequielalos@gmail.com>
2026-03-31 12:54:40 -0300

    Add sexo and libero fields to RotacionActualEntity

--- app/src/main/java/com/pilar/newcomapp/data/local/entity/RotacionActualEntity.kt
+++ app/src/main/java/com/pilar/newcomapp/data/local/entity/RotacionActualEntity.kt
@@ -8,10 +8,25 @@ data class RotacionActualEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val partidoId: Long,
+    // Nombres
     val posicion1: String = "",
     val posicion2: String = "",
     val posicion3: String = "",
     val posicion4: String = "",
     val posicion5: String = "",
-    val posicion6: String = ""
+    val posicion6: String = "",
+    // Sexo: "M" = Masculino, "F" = Femenino
+    val sexo1: String = "M",
+    val sexo2: String = "M",
+    val sexo3: String = "M",
+    val sexo4: String = "M",
+    val sexo5: String = "M",
+    val sexo6: String = "M",
+    // Libero
+    val libero1: Boolean = false,
+    val libero2: Boolean = false,
+    val libero3: Boolean = false,
+    val libero4: Boolean = false,
+    val libero5: Boolean = false,
+    val libero6: Boolean = false
 )
