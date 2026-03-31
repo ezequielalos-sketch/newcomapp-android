bc81756cb9c316d59beeca4aadde579987755a6b
ezequielalos-sketch <ezequielalos@gmail.com>
2026-03-31 12:56:41 -0300

    Bump DB version to 2 for new entity fields

--- app/src/main/java/com/pilar/newcomapp/data/local/NewcomDatabase.kt
+++ app/src/main/java/com/pilar/newcomapp/data/local/NewcomDatabase.kt
@@ -9,7 +9,7 @@ import com.pilar.newcomapp.data.local.entity.RotacionActualEntity
 
 @Database(
     entities = [PartidoEntity::class, RotacionActualEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class NewcomDatabase : RoomDatabase() {
